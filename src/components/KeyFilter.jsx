a5489c25572852ff65650adcff25506fc28fb9d4
Sumeya <sumeyaibrahim72@gmail.com>
2024-01-03 01:58:49 +0300

    Add Keyfilter card component

---
+++ src/components/KeyFilter.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TbHeading } from 'react-icons/tb';
+
+export const KeyFilter = () => {
+    return (
+        <div className="w-5/12 h-80 border rounded-md shadow-lg ">
+            <div className="flex flex-col justify-start items-start ">
+                <h1 className="font-semibold text-black p-2">Add blocks</h1>
+                <p className="p-2">Keep typing to filter, or escape to exit </p>
+                <div className="flex w-full mt-2 flex-col gap-2 ">
+                    <p className="text-gray-500 p-2">
+                        Filtering key <span className="p-[2px] rounded-sm text-white bg-mainColor">1</span>
+                    </p>
+                    <div className="mt-2 flex flex-col">
+                        <div className="flex w-full justify-start gap-2 items-center p-1 bg-gray-100">
+                            <TbHeading className="text-4xl" />
+                            <div className="flex flex-col">
+                                <h1 className="text-black font-semibold text-md">Heading 1</h1>
+                                <p className="text-sm">shortcut: type # + space</p>
+                            </div>
+                        </div>
+                        <div className="flex w-full justify-start gap-2 items-center p-1">
+                            <TbHeading className="text-4xl" />
+                            <div className="flex flex-col">
+                                <h1 className="text-black font-semibold text-md">Expandaple Heading 1</h1>
+                                <p className="text-sm">shortcut: type `{'>>'}` + space</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Info } from './Info';
 import { Heading } from './Heading';
+import { KeyFilter } from './KeyFilter';
 
 export const Main = () => {
     return (
         <div className="flex flex-col items-center font-poppins w-full text-gray-400">
             <Info />
             <Heading />
+            <div className="w-7/12">
+                <KeyFilter />
+                {/* <div className="mt-9">
+                    <span>this is the typing input</span>
+                </div> */}
+            </div>
         </div>
     );
 };
